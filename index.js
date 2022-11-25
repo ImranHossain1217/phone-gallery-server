95e9e91abb209d43fd2319ada355ce4e74ebea03
Imran Hossain <imranhossainemon1717@gmail.com>
2022-11-26 01:02:59 +0600

    get category id

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 
 //middleware
@@ -29,6 +29,12 @@ async function run() {
       res.send(category);
     });
 
+    app.get("/category/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await categoryCollection.findOne(query);
+      res.send(result);
+    });
   } finally {
   }
 }
