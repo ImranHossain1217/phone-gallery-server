838bbf5dd8d22c29321cf773ea5e744321bf1475
Imran Hossain <imranhossainemon1717@gmail.com>
2022-11-26 00:34:08 +0600

    connect with mongo db

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 
 //middleware
 app.use(cors());
 app.use(express.json());
 
-app.get('/',(req,res)=> {
-    res.send('Phone gallery server Running..!!!');
+const uri = `mongodb+srv://${process.env.DB_User}:${process.env.DB_Password}@cluster0.vwrnpfj.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
 });
 
-app.listen(port,()=> {
-    console.log(`Phone Gallery server running on ${port}`);
+async function run() {
+  try {
+  } finally {
+  }
+}
+
+run().catch((err) => console.error(err));
+
+app.get("/", (req, res) => {
+  res.send("Phone gallery server Running..!!!");
+});
+
+app.listen(port, () => {
+  console.log(`Phone Gallery server running on ${port}`);
 });
